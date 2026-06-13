10dee5dc5d62f6712c0dd38669609ede4dee6bfc
Marvin0G <TitaniumxXxoriginal@gmail.com>
2026-06-13 23:08:09 +0100

    Update footer with current year and last modified date in forms.html and add corresponding JavaScript functionality

--- forms/script/forms.js
+++ forms/script/forms.js
@@ -1,3 +1,13 @@
+const currentYear = document.querySelector('.currentyear');
+const lastModified = document.querySelector('#lastmodified');
+
+const today = new Date();
+
+currentYear.textContent = today.getFullYear();
+
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
+
+
 // Custom Product Data Array
 const products = [
   {
